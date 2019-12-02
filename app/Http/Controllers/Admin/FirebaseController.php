c8863daae1f54827ca9e5acee6e34e8df464b4ba
Eduardo <edward.axl3@gmail.com>
2019-12-02 11:23:53 -0500

    Add FCM (send notification to all Android devices) correccion 3

--- app/Http/Controllers/Admin/FirebaseController.php
+++ app/Http/Controllers/Admin/FirebaseController.php
@@ -13,8 +13,10 @@ class FirebaseController extends Controller
 	    $recipients = User::whereNotNull('device_token')->pluck('device_token')->toArray();
 
 	    fcm()
-	   		->to($recipients) // $recipients must an array
-	    	->notification([
+		    ->to($recipients) // $recipients must an array
+		    ->priority('high')
+		    ->timeToLive(0)
+		    ->notification([
 		        'title' => $request->input('title'),
 		        'body' => $request->input('body'),
 		    ])
